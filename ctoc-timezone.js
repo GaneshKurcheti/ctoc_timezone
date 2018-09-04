329042694fe9ed9fdbe453dd738431fa020b455d
nagaganesh.kurcheti <kurchetinagaganeshsing@gmail.com>
2018-09-04 16:51:22 +0530

    Modified code and added comments

--- ctoc-timezone.js
+++ ctoc-timezone.js
@@ -1,18 +1,32 @@
 "use strict";
+
 CtoCTimezone = (function() {
+    /*
+    * Starts the conversion process.
+    */
     var initiateConversion = function(){
+      if (window.removeEventListener)
+        window.removeEventListener('load', process);
+      else if (window.detachEvent)
+        window.detachEvent('onload', process);
       nodesToConvert=document.querySelectorAll('[data-ctoc-timezone="server"]');
       for(i = 0; i < nodesToConvert.length; i++){
         convertToClient(nodesToConvert[i]);
       }
     };
+
+    /*  
+    * The actual method that handles the conversion of the server time to client.
+
+    * params node: represents any node that is needed to be converted   
+    */
     var convertToClient = function(node){
       node.innerHTML=""
       timeToConvert=node.getAttribute('data-ctoc-time');
       clientTime = new Date(timeToConvert);
       if(node.getAttribute('data-ctoc-req-zone')!="")
       {
-        // need to handle this to convert to zonal specific timezone 
+        // Need to handle this to convert to zonal specific Date Object 
         clientTime = node.getAttribute('data-ctoc-req-zone')
       }
       result=clientTime;
@@ -22,6 +36,17 @@ CtoCTimezone = (function() {
       node.innerHTML=result;
       node.setAttribute("data-ctoc-timezone","client");
     };
+
+    /*
+    * When user intends to send the date object generated to the other call back function.
+
+    * This is used if the user dont intend to get the converted timezone value just to the html page.
+
+    * Params callbackString: This is the required method to which the date object need to be sent.
+
+    * Params ctocTime: This the time object that is need to convert. 
+    */
+
     var executeCallback = function(callbackString,ctocTime){
       var namespacesCheck = callbackString.split(".");
       var functionToCall = namespacesCheck.pop();
@@ -32,9 +57,23 @@ CtoCTimezone = (function() {
       return context[functionToCall].apply(context, ctocTime);
     };
 
+    /*
+
+    * This object is exposed to external world to facilitate the instant load.
+
+    */
+
     var ctoCTimezone = {};
     ctoCTimezone.runInstaLoad =function(){
       initiateConversion();
     };
+    
+    // Convert elements on the window load. 
+
+    if (window.addEventListener)
+    window.addEventListener('load', initiateConversion);
+    else if (window.attachEvent)
+    window.attachEvent('onload', initiateConversion);
+
     return ctoCTimezone;
 })();
